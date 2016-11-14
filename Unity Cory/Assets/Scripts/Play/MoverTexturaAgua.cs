139fa09e7ab211ce7e3d4a0d716e730be2cc0361
JesusBachiller <chuse699612@gmail.com>
2016-11-14 13:18:51 +0100

    movimiento de la textura del agua

---
+++ Unity Cory/Assets/Scripts/Play/MoverTexturaAgua.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoverTexturaAgua : MonoBehaviour {
+
+    private float desplazaX = 0;
+    private float desplazaY = 0;
+
+	void FixedUpdate () {
+        desplazaX += 0.02f;
+        desplazaY += 0.01f;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(Mathf.Cos(desplazaX), Mathf.Sin(desplazaY) / 4);
+	}
+}
