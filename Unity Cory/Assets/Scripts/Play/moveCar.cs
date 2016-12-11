93062ecbd4bd20b34204096a05e1ffffef702788
alexgrao <alexgrao13@gmail.com>
2016-12-11 13:31:44 +0100

    Cambios Diseño Tutorial

---
+++ Unity Cory/Assets/Scripts/Play/moveCar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCar : MonoBehaviour
+{
+
+    private float Velocity;
+
+    void Start()
+    {
+        Velocity = 0.15f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x + Velocity, transform.position.y, transform.position.z);
+
+        if (transform.position.x >= 59f)
+        {
+            Velocity = 0;
+        }
+    }
+}
